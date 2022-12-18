5de4bf9ac9242512f7f09502bd26196be95f2b70
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-12-17 23:50:09 -0300

    abstracted function to use local storage state

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,17 +3,21 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  const [name, setName] = React.useState(
-    () => window.localStorage.getItem('name') || initialName
+function useLocalStorageState(key, defaultValue = '') {
+  const [state, setState] = React.useState(
+    () => window.localStorage.getItem(key) || defaultValue
   )
-
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
+  
   React.useEffect(() => {
-    window.localStorage.setItem('name', name)
-  }, [name])
+    window.localStorage.setItem(key, state)
+  }, [key, state])
+
+  return [state, setState]
+}
+
+function Greeting({initialName = ''}) {
+  
+  const [name, setName] = useLocalStorageState('name', initialName) 
 
   function handleChange(event) {
     setName(event.target.value)
