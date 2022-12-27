72f25b5899e5a5fed312aa994f8f35d3c07c7565
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-12-27 20:29:13 -0300

    storing states in an state object

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -14,24 +14,25 @@ import {
 } from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
-  const [status, setStatus] = React.useState('idle')
-  const [pokemon, setPokemon] = React.useState(null)
-  const [error, setError] = React.useState(null)
+  const [state, setState] = React.useState({
+    status: 'idle',
+    pokemon: null,
+    error: null,
+  })
+  const {status, pokemon, error} = state
 
   React.useEffect(() => {
     if (!pokemonName) {
       return
     }
-    setPokemon('pending')
+    setState({status: 'pending'})
     fetchPokemon(pokemonName).then(
-      pokemonData => {
-        setPokemon(pokemonData)
-        setStatus('resolved')
+      pokemon => {
+        setState({pokemon, status: 'resolved'})
       },
       error => {
-        setError(error)
-        setStatus('rejected')
-      }
+        setState({error, status: 'rejected'})
+      },
     )
   }, [pokemonName])
   
